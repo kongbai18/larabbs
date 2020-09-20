e6ac4250a2abddb7daeca5f259c7650c26ac6f47
vagrant <vagrant@homestead>
2020-09-20 14:54:07 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="#" target="_blank">Ligo</a> 设计和编码 <span></span>
+    </p>
+
+    <p class="float-right"><a href="">联系我们</a></p>
+  </div>
+</footer>
