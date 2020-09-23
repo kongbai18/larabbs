2cfabaa88e0a45238af3d7d882862bc58ff1fcdc
vagrant <vagrant@homestead>
2020-09-23 15:19:52 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'intriduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif,jpg|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -35,6 +36,7 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => '用户名',
+            'avatar' => '头像',
         ];
     }
 }
